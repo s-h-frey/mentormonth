51b44e0233b11db2e679d72d6b23090fdd7d80fe
Julian Kodlin <rolpirrow@julians-air.speedport_w_723v_1_45_000>
2018-04-09 16:59:03 +0200

    prototype for form connected to google sheet

---
+++ app.rb
@@ -0,0 +1,42 @@
+require("bundler")
+require 'sinatra'
+Bundler.require
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+#set the default orientation, so stuff like: <link rel="stylesheet" href="/public/app.css"> works
+set :public_folder, File.dirname(__FILE__)
+
+#connection to google sheets
+def worksheet
+    @session ||= GoogleDrive::Session.from_service_account_key("client_secret.json")
+    @spreadsheet ||= @session.spreadsheet_by_title("Mentormonth")
+    @worksheet ||= @spreadsheet.worksheets.first
+end
+
+get "/" do
+    erb :index
+end
+
+get '/form1' do
+  erb :form1
+end
+
+post "/output1" do
+    session[:name] = params["name"]
+    erb :form2
+end
+
+post "/output2" do
+    session[:email] = params["email"]
+    erb :form3
+end
+
+post "/output_final" do
+    new_row = [session[:name], session[:email], params["phone_number"], params["age"], params["degree"]]
+    worksheet.insert_rows(worksheet.num_rows+1, [new_row])
+    worksheet.save
+    erb :thanks
+end
